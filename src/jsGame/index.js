be8ac11065bd2743bdb3d203bb31b0d6aa5695a9
lvhongfa <hflv@91souban.com>
2017-08-06 22:17:21 +0800

    commit 封装监听键盘事件以及对键盘事件添加进入一个数组队列，通过按下和释放来更改true & false值，从而，使得true的代码执行，false则不执行任何代码

--- src/jsGame/index.js
+++ src/jsGame/index.js
@@ -5,6 +5,65 @@ var imageFromPath = function(path) {
   return image
 }
 
+// 定义生成canvas模块
+var Game = function() {
+  var g = {
+    actions: {},
+    keydowns: {},
+  }
+  // for make up canvas
+  var canvas = document.querySelector('#isCanvas')
+  var context = canvas.getContext('2d')
+  g.canvas = canvas 
+  g.context = context
+
+  
+  // events
+  /**
+   * 监听键盘注册事件，
+   * 如果按下键盘，那么让g的keydowns对象里面存储一个对应
+   * 事件的键名，并且对应的值置为true
+   * 
+   * 如果释放键盘，那么让g的keydowns对象里面存储的对应的
+   * 事件的键名的值更改为false
+   */
+  window.addEventListener('keydown',function(event){
+    g.keydowns[event.key] = true
+  })
+  window.addEventListener('keyup',function(event){
+    g.keydowns[event.key] = false
+  })
+
+  // 按键事件注册
+  /**
+   * 按键的事件注册，把key和callback传入，使得g的action键名为按下的key
+   * 并且对应的值成为传入的回调函数（也就是要执行的代码）
+   */
+  g.registerAction = function(key, callback) {
+    g.actions[key] = callback
+  }
+  // timer
+  setInterval(function(){
+    // update event
+    var actions = Object.keys(g.actions)
+
+    for (var i = 0; i < actions.length; i++) {
+      var key = actions[i];
+      if(g.keydowns[key]) {
+        // 如果按键被按下，调用注册的action
+        g.actions[key]()
+      }
+    }
+    // update x & y
+    g.update()
+    // clear canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // refresh draw
+    g.draw()
+  }, 1000/60)
+  return g
+}
+
 // 定义挡板模块
 var Paddle = function() {
   var image = imageFromPath('paddle.png')
@@ -28,53 +87,36 @@ var init = function() {
 
   // for util log
   var log = console.log.bind(console)
-
-  // for make up canvas
-  var canvas = document.querySelector('#isCanvas')
-  var context = canvas.getContext('2d')
-
-  // 挡板变量坐标
-  var x = 150
-  var y = 700
-  var speed = 8
-
-  // 按键状态
-  var leftDown = false
-  var rightDown = false
+  // 生成canvas模块
+  var game = Game()
 
   // for make up image into canvas
   var paddle = Paddle()
 
-  // event
-  window.addEventListener('keydown', function(event){
-    var _k = event.key
-    if(_k == 'ArrowLeft') {
-      leftDown = true
-    }else if (_k == 'ArrowRight') {
-      rightDown = true
-    }
+  game.registerAction('ArrowLeft' , function(){
+    paddle.moveLeft()
   })
-  window.addEventListener('keyup', function(event){
-    var _k = event.key
-    if(_k == 'ArrowLeft') {
-      leftDown = false
-    }else if (_k == 'ArrowRight') {
-      rightDown = false
-    }
+  game.registerAction('ArrowRight' , function(){
+    paddle.moveRight()
   })
+  game.update = function() {
+  }
+  game.draw = function() {
+    game.context.drawImage(paddle.img, paddle.x, paddle.y)
+  }
 
   // timer
-  setInterval(function(){
-    // update x & y
-    if (leftDown) {
-      paddle.moveLeft()
-    }else if(rightDown){
-      paddle.moveRight()
-    }
-    // refresh draw
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(paddle.img, paddle.x, paddle.y)
-  }, 1000/60)
+  // setInterval(function(){
+  //   // update x & y
+  //   if (leftDown) {
+  //     paddle.moveLeft()
+  //   }else if(rightDown){
+  //     paddle.moveRight()
+  //   }
+  //   // refresh draw
+  //   context.clearRect(0, 0, canvas.width, canvas.height);
+  //   context.drawImage(paddle.img, paddle.x, paddle.y)
+  // }, 1000/60)
 
 }
 
